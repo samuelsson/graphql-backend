339601bf9ac1e65f17143bda7292b3e969e4e892
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2019-04-12 23:57:32 +0200

    Make database connect a normal function instead of class

--- src/database.ts
+++ src/database.ts
@@ -1,26 +1,21 @@
 import mongoose from 'mongoose';
 
-const database = class Database {
-    constructor(private dbHost: string, private dbName: string, private dbPort: number) {
-        this.connect(dbHost, dbName, dbPort);
-        this.setEvents();
-    }
+const connect = (dbHost: string, dbName: string, dbPort: number): void => {
+    const url = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    const options = {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    };
 
-    private connect(dbHost: string, dbName: string, dbPort: number) {
-        const url = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    mongoose
+        .connect(url, options)
+        .then(() => console.info(`Successfully connected to database ${dbHost}:${dbPort}/${dbName}`))
+        .catch(error => {
+            console.error('Error connecting to database: ', error);
+            return process.exit(1);
+        });
 
-        mongoose
-            .connect(url, { useNewUrlParser: true })
-            .then(() => console.info(`Successfully connected to database ${dbHost}:${dbPort}/${dbName}`))
-            .catch(error => {
-                console.error('Error connecting to database: ', error);
-                return process.exit(1);
-            });
-    }
-
-    private setEvents() {
-        mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    }
+    mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 };
 
-export default database;
+export default { connect };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import cors from 'cors';
 import Server from './server';
-import Database from './database';
+import database from './database';
 import graphql from './graphql';
 import config from './config';
 
+// Create a connection to the MongoDB
+database.connect(config.dbHost, config.dbName, config.dbPort);
+
+// Initiate a new Server object
 const server = new Server(config.port);
-const database = new Database(config.dbHost, config.dbName, config.dbPort);
 
 const corsOptions = {
     origin: config.corsOrigin,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+// Add middleware to our server
 server.app.use(cors(corsOptions));
 server.app.use('/api', graphql);
 
